9518e3ebb4a1f9c559cf2c048fce116442b9e08f
sarinhamartins <sariinha.martins@gmail.com>
2018-10-31 09:44:38 -0300

    exercício do CPF corrigido

--- 02-dom/main.js
+++ 02-dom/main.js
@@ -6,27 +6,21 @@ input.addEventListener("focus", function(){
     setTimeout(function() {
         that.setSelectionRange(0, 0);
     }, 10);
-    
 })
 
 input.addEventListener("blur", function(){
     this.value = "";
 })
 
-// input.addEventListener("keypress", function(e){
-//     e.preventDefault();
-//     cpf = Number.isInteger(parseInt(e.key));
-//     if (cpf) {
-//         input.value = input.value.replace(/_/, e.key);
-//     }
-// })
-
-input.addEventListener("keyup", function(e){
+input.addEventListener("keydown", function(e){
     e.preventDefault();
     if ("0123456789".indexOf(e.key) !== -1 && this.value.indexOf("_") !== -1) {
         this.value = this.value.replace(/_/, e.key);
         const next_index = this.value.indexOf("_");
         this.setSelectionRange(next_index, next_index);
+    } else if (e.key === "Backspace") {
+        this.value = this.value.replace(/(\d$)|(\d(?=\D+$))/, "_");
+        const next_index = this.value.indexOf("_");
+        this.setSelectionRange(next_index, next_index);
     }
-
 })
