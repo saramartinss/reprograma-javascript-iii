befc3e0dcbee199cd3d9f284fb145c334db7b754
sarinhamartins <sariinha.martins@gmail.com>
2018-11-01 17:08:51 -0300

    exercício consumindo api do youtube

---
+++ 04-apis/README.md
@@ -0,0 +1,3 @@
+### Youtube
+
+Para esse projeto funcionar corretamente você deve duplicar o arquivos `credentials.sample.js`, mudar o nome dele para `credentials.js` e colocar a sua chave de API 
---
+++ 04-apis/main.js
@@ -0,0 +1,20 @@
+const input_busca = document.getElementById("inputBusca");
+const div = document.getElementById("results");
+
+input_busca.addEventListener("keyup", function() {
+  div.innerHTML = "<img id='loading' src='loading.gif'>";
+  get_data(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.value}&type=video&key=${gkey}`)
+  .then(function (data) {
+    const obj = JSON.parse(data);
+    div.innerHTML = "";
+    for(const item of obj.items) {
+      div.innerHTML += `<a href="https://www.youtube.com/watch?v=${item.id.videoId}"><div><img src="${item.snippet.thumbnails.default.url}"></div><div><h1>${item.snippet.title}</h1><p>${item.snippet.description}</p></div></a>`;
+    }
+    if (input_busca.value.length === 0) {
+      div.innerHTML = "";
+    }
+  })
+  .catch(function (error) {
+    div.innerHTML = `<p>${error}</p>`
+  })
+})
