1eb831d303a171eb089434c8129230e9a9111234
sarinhamartins <sariinha.martins@gmail.com>
2018-11-05 09:13:12 -0200

    inclui o else da correção

--- 04-apis/main.js
+++ 04-apis/main.js
@@ -7,8 +7,12 @@ input_busca.addEventListener("keyup", function() {
   .then(function (data) {
     const obj = JSON.parse(data);
     div.innerHTML = "";
-    for(const item of obj.items) {
-      div.innerHTML += `<a href="https://www.youtube.com/watch?v=${item.id.videoId}"><div><img src="${item.snippet.thumbnails.default.url}"></div><div><h1>${item.snippet.title}</h1><p>${item.snippet.description}</p></div></a>`;
+    if (data.items.length > 0) {
+      for(const item of obj.items) {
+        div.innerHTML += `<a href="https://www.youtube.com/watch?v=${item.id.videoId}"><div><img src="${item.snippet.thumbnails.default.url}"></div><div><h1>${item.snippet.title}</h1><p>${item.snippet.description}</p></div></a>`;
+      }
+    } else {
+      div.innerHTML = "<p>Nenhum resultado encontrado</p>";
     }
     if (input_busca.value.length === 0) {
       div.innerHTML = "";
